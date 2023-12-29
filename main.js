1faaf139885ef007a566786a1c54195a3e313d22
Lucas Chaves <lucasvchaves@gmail.com>
2023-12-29 17:12:40 -0300

    Adiciona a funcao preencheLista

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 import { getEstoque, limpaEstoque, transacaoNoEstoque } from "./estoque";
 
+const olJoao = document.querySelector("#joao");
+const olMaria = document.querySelector("#maria");
+
+function preencheLista(pessoa, lista){
+    lista.innerHTML = "";
+    for(let i = 0; i < pessoa.length; i++){
+        const frutas = pessoa[i];
+        const eLi = document.createElement('li');
+        eLi.innerText = `${frutas.tipo}: ${frutas.qtd}`;
+        lista.append(eLi); 
+    }
+}
